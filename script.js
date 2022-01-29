01238813e119677241ba21465e74ac35ec9da19f
Izik-Martinez <izikma17@yahoo.com>
2022-01-29 09:10:20 -0500

    Wrote function for (random)  computer choice

---
+++ script.js
@@ -0,0 +1,50 @@
+// function getRandomInt (max) {
+
+// return Math.floor(Math.random() * max);
+// }
+
+// console.log(getRandomInt(3));
+
+// console.log(getRandomInt(1));
+
+// console.log(Math.random());
+
+
+
+// function   name_of_function   (List of parameters to the function)
+
+// // Parameters, in a function call, are the function's arguments.
+// // Parameters are variables listed as a part of the function definition
+
+// function argCheck(parameter1, parameter2, parameter 3){
+
+// console.log(parameter1 + parameter2 + parameter3);
+
+// }
+
+
+
+// functions allow us to take our scripts and bundle them into little packages
+// that we can use over and over again without having to rewrite or change the code
+
+// function favoriteAnimal(animal) {
+
+// console.log(animal + "is my favorite animal!")
+
+// }
+
+// favoriteAnimal("Goat")
+
+
+
+
+
+function playHuman() {
+
+let options = ["rock", "paper", "scissors"];
+let chosenOption = options[Math.floor(Math.random()*options.length)];
+alert("Computer choice is " + chosenOption);
+
+
+}
+
